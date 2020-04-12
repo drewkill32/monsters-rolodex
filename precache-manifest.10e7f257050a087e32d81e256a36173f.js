3bb186453a1dcb0a5d3ec275b347b2ba5891251f
Drew Killion <adk@killion.me>
2020-04-11 21:02:37 -0400

    Updates

---
+++ precache-manifest.10e7f257050a087e32d81e256a36173f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e553f86b381e236b32625680fc4c02db",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a93af934e7b19274d4b6",
+    "url": "/monsters-rolodex/static/css/main.4cd1ce91.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a93af934e7b19274d4b6",
+    "url": "/monsters-rolodex/static/js/main.620ead17.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
